13d157478d2fabbaa34020f13d7342712fefada6
hkucherera@outlook.com <hkucherera@outlook.com>
2025-08-06 18:44:04 +0000

    Added Payments

---
+++ models/staging/stg_payments.sql
@@ -0,0 +1,21 @@
+with 
+
+source as (
+  select * from {{ source('ecom', 'raw_payments') }}
+),
+
+staged as (
+  
+  select
+      id as payment_id,
+      orderid as order_id,
+      payment_method as payment_method,
+      status,
+  
+      -- amount is stored in cents, convert it to dollars
+      amount / 100 as amount,
+      created as created_at
+  from source
+)
+
+select * from staged
